2081a807ae266cee7f62a9c942b1b81b389b5f1a
scarfase <scarfase1989@gmail.com>
2021-10-31 12:01:27 +0600

    add types package

---
+++ pkg/types/types.go
@@ -0,0 +1,47 @@
+package types
+
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы)
+type Money int64
+
+// Currency представляет код валюты
+type Currency string
+
+// Category представляет собой категорию, в которой был совершен платеж
+type Category string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+// PAN представляет номер карты
+type PAN string
+
+// Payment представляет информацию о платеже.
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+}
+
+// Card представляет информацию о платежной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+// PaymentSource источник оплаты
+type PaymentSource struct {
+	Type    string // "card"
+	Number  string // номер вида "5058 xxxx xxxx 8888"
+	Balance Money
+}
